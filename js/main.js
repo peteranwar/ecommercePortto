d94c7b03a8a95d0c63087c5fc077a285009f383c
Your Name <you@example.com>
2020-06-14 01:59:15 +0200

    finish but not completely

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
-console.log("testt")
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 5000,
+  },
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    }
+  }
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
+
+
+const toTop = document.querySelector('.scrolltop');
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 450) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+
+})
+
