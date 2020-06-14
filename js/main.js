107fa7237e55c8092ff3408f31985428887c4334
Your Name <you@example.com>
2020-06-15 00:36:06 +0200

    ssss

--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper(".swiper-container", {
   slidesPerView: 5,
   spaceBetween: 30,
-  autoplay: {
-    delay: 5000,
-  },
-  direction: getDirection(),
+  autoplay: true,
+  // direction: getDirection(),
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
     1024: {
       slidesPerView: 5,
       spaceBetween: 30,
     },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
   },
-  on: {
-    resize: function () {
-      swiper.changeDirection(getDirection());
-    }
-  }
+  // on: {
+  //   resize: function () {
+  //     swiper.changeDirection(getDirection());
+  //   }
+  // }
 });
 
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+// function getDirection() {
+//   var windowWidth = window.innerWidth;
+//   var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
 
-  return direction;
-}
+//   return direction;
+// }
 
-
-const toTop = document.querySelector('.scrolltop');
+const toTop = document.querySelector(".scrolltop");
 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 450) {
@@ -50,6 +51,13 @@ window.addEventListener("scroll", () => {
   } else {
     toTop.classList.remove("active");
   }
+});
 
-})
-
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 30) {
+    nav.classList.add("nav__scroll");
+  } else {
+    nav.classList.remove("nav__scroll");
+  }
+});
