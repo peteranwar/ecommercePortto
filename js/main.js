53f48e8f0c76ab946face15297a0f9d996213322
Your Name <you@example.com>
2020-06-19 20:16:47 +0200

    add more sections

--- js/main.js
+++ js/main.js
@@ -31,8 +31,7 @@ var swiper = new Swiper(".swiper-container", {
     300: {
       slidesPerView: 1,
       spaceBetween: 30,
-    }
-
+    },
   },
   // on: {
   //   resize: function () {
@@ -65,4 +64,115 @@ window.addEventListener("scroll", () => {
   } else {
     nav.classList.remove("nav__scroll");
   }
+});
+
+//
+// var galleryThumbs = new Swiper('.gallery-thumbs', {
+//   spaceBetween: 10,
+//   slidesPerView: 4,
+//   freeMode: true,
+//   watchSlidesVisibility: true,
+//   watchSlidesProgress: true,
+// });
+// var galleryTop = new Swiper('.gallery-top', {
+//   spaceBetween: 10,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   thumbs: {
+//     swiper: galleryThumbs,
+//   }
+// });
+
+// $('#zoom_05').elevateZoom( {
+//   zoomType				: "inner",
+//   cursor: "crosshair"
+// });
+// $('#zoom_06').elevateZoom( {
+//   zoomType				: "inner",
+//   cursor: "crosshair"
+// });
+// $('#zoom_07').elevateZoom( {
+//   zoomType				: "inner",
+//   cursor: "crosshair"
+// });
+// $('#zoom_08').elevateZoom( {
+//   zoomType				: "inner",
+//   cursor: "crosshair"
+// });
+
+// Focus.init({
+//   elementID: '#zoomzo',
+//   zoomFactor: '250%'
+// });
+// let config = {
+//   imageSrc: './imgs/product-1-big.jpg',
+//   parentElement: 'demo',
+//   zoomFactor: '250%',
+//   smoother: true,
+//   width: '100%', // Scale to parent component by default
+//   height: '66.7%', // Scale to percent of height by default
+//   cursor: '', // Leave blank for default cursor
+//   displayLoc: true,
+//   displayZoom: true,
+//   zoomOnScroll: true,
+// };
+// new FocusImage(config);
+
+// var options = {
+//   width: 600,
+//   height:400,
+//   zoomWidth: 400,
+//   offset: {vertical: 0, horizontal: 10}
+// };
+// new ImageZoom(document.getElementById("img-container"), options);
+$(function () {
+  $("#exzoom").exzoom({
+    autoPlay: false,
+  });
+});
+
+var swiper_2 = new Swiper(".swiper-container_2", {
+  autoplay: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper_3 = new Swiper(".swiper-container_3", {
+  slidesPerView: 4,
+  spaceBetween: 40,
+  // init: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+  },
 });
