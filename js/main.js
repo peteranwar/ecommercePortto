25fc2496130760333a778afbf295dec715596169
Your Name <you@example.com>
2020-06-30 04:56:01 +0200

    final updates

--- js/main.js
+++ js/main.js
@@ -33,20 +33,11 @@ var swiper = new Swiper(".swiper-container", {
       spaceBetween: 30,
     },
   },
-  // on: {
-  //   resize: function () {
-  //     swiper.changeDirection(getDirection());
-  //   }
-  // }
+  
 });
 
-// function getDirection() {
-//   var windowWidth = window.innerWidth;
-//   var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
 
-//   return direction;
-// }
-
+// show button to top by add class active
 const toTop = document.querySelector(".scrolltop");
 
 window.addEventListener("scroll", () => {
@@ -57,6 +48,9 @@ window.addEventListener("scroll", () => {
   }
 });
 
+
+
+// change nav when scroll
 const nav = document.querySelector("nav");
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 30) {
@@ -66,73 +60,16 @@ window.addEventListener("scroll", () => {
   }
 });
 
-//
-// var galleryThumbs = new Swiper('.gallery-thumbs', {
-//   spaceBetween: 10,
-//   slidesPerView: 4,
-//   freeMode: true,
-//   watchSlidesVisibility: true,
-//   watchSlidesProgress: true,
-// });
-// var galleryTop = new Swiper('.gallery-top', {
-//   spaceBetween: 10,
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-//   thumbs: {
-//     swiper: galleryThumbs,
-//   }
-// });
 
-// $('#zoom_05').elevateZoom( {
-//   zoomType				: "inner",
-//   cursor: "crosshair"
-// });
-// $('#zoom_06').elevateZoom( {
-//   zoomType				: "inner",
-//   cursor: "crosshair"
-// });
-// $('#zoom_07').elevateZoom( {
-//   zoomType				: "inner",
-//   cursor: "crosshair"
-// });
-// $('#zoom_08').elevateZoom( {
-//   zoomType				: "inner",
-//   cursor: "crosshair"
-// });
-
-// Focus.init({
-//   elementID: '#zoomzo',
-//   zoomFactor: '250%'
-// });
-// let config = {
-//   imageSrc: './imgs/product-1-big.jpg',
-//   parentElement: 'demo',
-//   zoomFactor: '250%',
-//   smoother: true,
-//   width: '100%', // Scale to parent component by default
-//   height: '66.7%', // Scale to percent of height by default
-//   cursor: '', // Leave blank for default cursor
-//   displayLoc: true,
-//   displayZoom: true,
-//   zoomOnScroll: true,
-// };
-// new FocusImage(config);
-
-// var options = {
-//   width: 600,
-//   height:400,
-//   zoomWidth: 400,
-//   offset: {vertical: 0, horizontal: 10}
-// };
-// new ImageZoom(document.getElementById("img-container"), options);
+// make zoom in product
 $(function () {
   $("#exzoom").exzoom({
     autoPlay: false,
   });
 });
 
+
+// code swiper_2 
 var swiper_2 = new Swiper(".swiper-container_2", {
   autoplay: true,
   navigation: {
@@ -141,6 +78,8 @@ var swiper_2 = new Swiper(".swiper-container_2", {
   },
 });
 
+
+// code swiper_3 
 var swiper_3 = new Swiper(".swiper-container_3", {
   slidesPerView: 4,
   spaceBetween: 40,
@@ -176,3 +115,56 @@ var swiper_3 = new Swiper(".swiper-container_3", {
     },
   },
 });
+
+
+
+
+// pop up add to cart 
+const btnsAdd = document.querySelectorAll('.btn-add');
+const popUp = document.querySelector('.pop-up');
+const continu = document.querySelector('.continue');
+const popUpContent = document.querySelector('.pop-up-content');
+
+btnsAdd.forEach((btn)=> {
+ btn.addEventListener("click", () => {
+   popUp.style.display = "flex";
+ })
+});
+
+continu.addEventListener("click", () => {
+  popUp.style.display = "none";
+
+});
+popUp.addEventListener("click", () => {
+  popUp.style.display = "none";
+  
+});
+popUpContent.addEventListener("click", (e) => {
+  e.stopPropagation();
+  
+})
+
+
+// decrease and increase items
+
+const decrease = document.querySelector('.minus');
+const increase = document.querySelector('.plus');
+
+increase.addEventListener('click', ()=> {
+  let inpvalue = parseInt(document.querySelector('#value').value);
+
+  inpvalue++;
+  //  parseInt(inpvalue.value)  += count ;
+  document.querySelector('#value').value = inpvalue;
+
+})
+decrease.addEventListener('click', ()=> {
+  let inpvalue = parseInt(document.querySelector('#value').value);
+
+  if (inpvalue > 1) {
+    inpvalue--;
+  // value.value = count;
+  document.querySelector('#value').value = inpvalue
+
+  }
+})
